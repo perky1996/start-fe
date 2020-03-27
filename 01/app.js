3ce07da91d14cea6d59b9a06047b4d482626fa57
perky1996 <37091525+perky1996@users.noreply.github.com>
2020-03-27 15:47:43 +0900

    01 수업과제

---
+++ 01/app.js
@@ -0,0 +1,69 @@
+console.log('app.js');
+
+// var name = 'FE';
+// var age = 28;
+// var message = 'hello' + name;
+
+// console.log(name, age, message);
+
+// //var commentCount = 20;
+
+// /*
+// document.getElementById('wrap').innerHTML = content;
+// */
+
+// var count = 5;
+
+// var nick = "my nick is 'fe'";
+// var homepage = 'www.kakaocorp.com';
+
+// var isMan = true;
+
+// console.log(nick == '');
+
+// console.log(!!'test');
+
+// var log = function(str) {
+//     console.log(str);
+// }
+
+// var image = {};
+
+// image.width = 1;
+// image.height = 2;
+
+// image = [];
+
+// image[0] = '1.jpg';
+// image[1] = '2.jpg';
+
+// var nick = null;
+// var nick;
+
+// console.log(1 + 2);
+// console.log('a' + 'b');
+// console.log(1 + '4');
+
+// '' == '0';
+// 0 == '';
+// 0 === '';
+
+// if (isHero && isHulk) {
+
+// }
+
+// if (isHero || isHulk) {
+
+// }
+
+// if (!isHero) {
+
+// }
+
+// delete image.width;
+
+// image.width = null;
+
+// console.log(typeof (1 +'4'));
+
+// typeof foo !== 'undefined';
