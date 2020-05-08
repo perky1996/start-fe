57b418afa50fe0517c632fa41c7ec8f5c998b96a
perky1996 <37091525+perky1996@users.noreply.github.com>
2020-05-08 16:00:09 +0900

    07

---
+++ 07/test.js
@@ -0,0 +1,19 @@
+const str = 'aaa';
+const obj = {
+  a: 1,
+  b: 1,
+  c: 1,
+  d: 1,
+  f: 1,
+  a: 1,
+  b: 1,
+  c: 1,
+  d: 1,
+  f: 1,
+  a: 1,
+  b: 1,
+  c: 1,
+  d: 1,
+  f: 1,
+};
+console.log(obj);
